ebd1e25a9606e1145fa878ae4e000badec071851
叶俊权 <425527169@qq.com>
2017-01-10 17:44:39 +0800

    demo

---
+++ SocketDemo/SocketDemo.FirstCustomer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketDemo.FirstCustomer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("请输入服务端Ip:");
+            string serviceIp = Console.ReadLine();
+            Console.WriteLine("请输入服务端端口:");
+            int servicePort = Convert.ToInt32(Console.ReadLine());
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(serviceIp), servicePort);
+            try
+            {
+                client.Connect(ipPoint);
+            }
+            catch(SocketException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            byte[] receiveBytes = new byte[1024];
+            int receivedLength = client.Receive(receiveBytes);
+            string receivedContent = Encoding.UTF8.GetString(receiveBytes, 0, receivedLength);
+            Console.WriteLine(receivedContent);
+            while (true)
+            {
+                Console.WriteLine("请输入要发送的内容：");
+                string input = Console.ReadLine();
+                if (string.Equals(input, "exists", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+                client.Send(Encoding.UTF8.GetBytes(input));
+                receivedLength = client.Receive(receiveBytes);
+                receivedContent = Encoding.ASCII.GetString(receiveBytes, 0, receivedLength);
+                Console.WriteLine(receivedContent);
+            }
+            Console.WriteLine("disconnect from sercer");
+            client.Shutdown(SocketShutdown.Both);
+            client.Close();
+        }
+    }
+}
---
+++ SocketDemo/SocketDemo.FirstService/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketDemo.FirstService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, 0209);
+            Socket serviceSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serviceSocket.Bind(ipPoint);
+            serviceSocket.Listen(58);
+            Console.WriteLine("wating for a client...");
+
+            Socket client = serviceSocket.Accept();
+            IPEndPoint clientPoint = (IPEndPoint)client.RemoteEndPoint;
+            Console.WriteLine(clientPoint.Address + "已连接");
+            string defaultContent = "Hello";
+            byte[] data = new byte[1024];
+            data = Encoding.UTF8.GetBytes(defaultContent);
+            client.Send(data, data.Length, SocketFlags.None);
+            int receivedLength = 0;
+            while (true)
+            {
+                var receiveData = new byte[1024];
+                receivedLength = client.Receive(receiveData);
+                if (receivedLength == 0)
+                {
+                    break;
+                }
+                var receivedContent = Encoding.UTF8.GetString(receiveData);
+                Console.WriteLine(clientPoint.Address + "接收内容:" + receivedContent);
+            }
+            Console.WriteLine(clientPoint.Address + "已断开");
+            client.Shutdown(SocketShutdown.Both);
+            client.Close();
+        }
+    }
+}
